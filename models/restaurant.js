f377f8e246b12e312cc6d10cfd03ea64c372c301
Stacey Yuan <staceyyuan75@gmail.com>
2021-03-27 15:58:21 +0800

    adjust schema

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,29 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
-  name: {
-    type: String, // 資料型別是字串
-    required: true, // 這是個必填欄位
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    required: true,
-  },
-  google_map: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  name_en: String,
+  category: { type: String, required: true },
+  image: String,
+  location: { type: String, required: true },
+  phone: String,
+  google_map: String,
+  rating: Number,
+  description: String,
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
